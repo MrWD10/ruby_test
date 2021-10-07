ffdda3b6c48c6b759ba1384864f9333dba0f2cf8
MrWD10 <waleedakhtar484@gmail.com>
2021-10-07 01:26:19 -0400

    section 6 completed

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,5 +37,18 @@ class PortfoliosController < ApplicationController
         end
     end
     
+    def show
+        @portfolio_item = Portfolio.find(params[:id]) 
+    end
+
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id]) 
+
+        @portfolio_item.destroy
+        respond_to do |format|
+          format.html { redirect_to portfolios_url, notice: "Portfolio item was successfully deleted." }
+        end
+      end
+
 
 end
